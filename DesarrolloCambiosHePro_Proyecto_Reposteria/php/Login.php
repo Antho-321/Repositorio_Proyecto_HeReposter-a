1b0675edec06ebcdf326e2cc1530bf63f3442abd
ZeroUnoDLC <El correo que utilizaron para crear su cuenta en github>
2023-02-10 13:29:27 -0500

    intento de login

--- DesarrolloCambiosHePro_Proyecto_Reposteria/php/Login.php
+++ DesarrolloCambiosHePro_Proyecto_Reposteria/php/Login.php
@@ -5,15 +5,20 @@ $connection = new Conexion;
 //Crea variables para almacenar lo del usuario
 $correo = $_POST['Correo'];
 $contraseña = $_POST['Contraseña'];
-//Crea variables para almacenar lo de la consulta
+//Crea variables para almacenar lo de la consulta y operar con ellos
 $consultemail = $connection->OperSql("SELECT `Email` FROM `usuario` WHERE `Email`= '$correo'");
 $consultpass = $connection->OperSql("SELECT `Password` FROM `usuario` WHERE `Email`= '$correo'");
 $email = mysqli_fetch_array($consultemail);
 $pass = mysqli_fetch_array($consultpass);
-if($email['Email']==$correo && $pass['Password']==$contraseña){
-    echo "Login correcto";
-}else{
-    echo "Mejor matate";
+if ($email!=null) {
+    if($email['Email']==$correo && $pass['Password']==$contraseña){
+        echo "Login correcto";
+    }else{
+         echo "contraseña incorrecta";
+    }
+    
+} else {
+    echo "No existe alguna cuenta registrada con ese correo";
 }
-
+$connection->closeConnection();
 ?>
