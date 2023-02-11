f8aabf0a3d3d4daaff017e0dcfe014d8f416b73c
Antho-321 <anthonyluisluna225@gmail.com>
2023-02-11 13:48:25 -0500

    cambio funcional

--- DesarrolloCambiosHePro_Proyecto_Reposteria/FINAL_TEST/enviar_correo.php
+++ DesarrolloCambiosHePro_Proyecto_Reposteria/FINAL_TEST/enviar_correo.php
@@ -1,14 +1,10 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  /*
+  
   $para = $_POST["para"];
   $asunto = "EN OTRA PRUEBA";
   $cuerpo = "2SEGUIMOS TESTEANDO";
   $salida = shell_exec('node ULTIMO_TEST.js "' . $para . '" "' . $asunto . '" "' . $cuerpo . '"');
-  */
-  
-  $v8 = new V8Js();
-  $js = 'console.log("Hola, mundo!");';
-  $v8->executeString($js);
+  echo "<h1>INGRESE CORREO<h1>";
 }
 ?>
