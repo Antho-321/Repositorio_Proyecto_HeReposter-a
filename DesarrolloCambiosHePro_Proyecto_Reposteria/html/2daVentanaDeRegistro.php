8cbf9293bdcdedbc104daabf3d47ddd8d1a432b1
ZeroUnoDLC <El correo que utilizaron para crear su cuenta en github>
2023-02-11 13:38:22 -0500

    cambio a ventana de registro en paso verificación

--- DesarrolloCambiosHePro_Proyecto_Reposteria/html/2daVentanaDeRegistro.php
+++ DesarrolloCambiosHePro_Proyecto_Reposteria/html/2daVentanaDeRegistro.php
@@ -1,3 +1,21 @@
+<?php
+//Parte de registro
+//Todo lo que envía el post a este lugar
+$cedula = $_POST['Cedula'];
+$nombre = $_POST['Nombre'];
+$apellido = $_POST['Apellido'];
+$direccion = $_POST['Direccion'];
+$correo = $_POST['Correo'];
+$contraseña = ($_POST['Contraseña']) ;
+$Rep_contraseña = $_POST['Rep_contraseña'];
+//Mete todo en este campo
+if($contraseña!=$Rep_contraseña){
+    echo '<script>
+    window.alert("Las contraseñas no coinciden"); 
+    window.location = "../html/Index.php";
+    </script>';
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
---
+++ DesarrolloCambiosHePro_Proyecto_Reposteria/php/CorreoConfirmación.php
@@ -0,0 +1,7 @@
+<?php
+// require("./Conexion.php");
+
+// $connection = new Conexion;
+// $connection->OperSql("INSERT INTO `cliente`(`Cedula`, `Nombre`, `Apellido`, `Direccion`) VALUES ('$cedula','$nombre' ,'$apellido','$direccion')");
+// $connection->OperSql("INSERT INTO `usuario`(`Cedula`, `Email`,  `Password`) VALUES ('$cedula','$correo','$contraseña')");
+//$connection->closeConnection();
