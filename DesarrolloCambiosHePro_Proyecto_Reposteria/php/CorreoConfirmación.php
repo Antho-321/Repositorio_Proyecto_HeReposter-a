fb62e126852d011ca5b682e127405b3d3a52be77
ZeroUnoDLC <El correo que utilizaron para crear su cuenta en github>
2023-02-15 17:38:42 -0500

    Conectado el ingreso a administrador

--- DesarrolloCambiosHePro_Proyecto_Reposteria/php/CorreoConfirmación.php
+++
@@ -1,7 +0,0 @@
-<?php
-// require("./Conexion.php");
-
-// $connection = new Conexion;
-// $connection->OperSql("INSERT INTO `cliente`(`Cedula`, `Nombre`, `Apellido`, `Direccion`) VALUES ('$cedula','$nombre' ,'$apellido','$direccion')");
-// $connection->OperSql("INSERT INTO `usuario`(`Cedula`, `Email`,  `Password`) VALUES ('$cedula','$correo','$contraseña')");
-//$connection->closeConnection();
