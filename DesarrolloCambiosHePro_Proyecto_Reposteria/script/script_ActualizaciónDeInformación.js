d38d68472a8ca44e8440ff42ab2c9f8a2f392c18
Antho-321 <anthonyluisluna225@gmail.com>
2023-02-22 20:10:14 -0500

    cambios

--- DesarrolloCambiosHePro_Proyecto_Reposteria/script/script_ActualizaciónDeInformación.js
+++ DesarrolloCambiosHePro_Proyecto_Reposteria/script/script_ActualizaciónDeInformación.js
@@ -9,39 +9,11 @@ let btnEnviar;
 let txtO = document.querySelector("label[for='ingreso_enlace']");
 window.onload = AgregarContenido("");
 div_fila.className = "fila";
-
-
 function AgregarContenido(CategoríaSeleccionada) {
     seccion_productos = document.getElementById("seccion_productos");
-    //let div, imagen, h3, a;
-    //let div_aux = document.createElement("div");
-    if (CategoríaSeleccionada == "") {
-        num_productos = 12;
-    } else {
-        /* 
-        ---------------------------------------------------------------------------------------------
-            num_productos sería el resultado de la consulta:
-        ---------------------------------------------------------------------------------------------
-            SELECT COUNT(*) FROM producto WHERE Categoría=CategoríaSeleccionada;
-        ---------------------------------------------------------------------------------------------
-            EL SIGUIENTE CÓDIGO ES SOLO DE PRUEBA:        
-        */
-        num_productos = 4;
-    }
-
-    //for (let i = 1; i <= num_productos; i++) {
-
-    //div = document.createElement("div");
-    //imagen = document.createElement("img");
-    //h3 = document.createElement("h3");
-    //a = 15.0;
-    //x = document.body.getBoundingClientRect().width;
     if (CategoríaSeleccionada == "") {
         let myData = myAsyncFunction("");
-
         myData.then(result => {
-            console.log(result);
-
             let div_aux = document.createElement("div");
             for (let i = 0; i < result.length; i++) {
                 let a = 15.0;
@@ -60,38 +32,7 @@ function AgregarContenido(CategoríaSeleccionada) {
             seccion_productos.appendChild(div_aux);
         }
         );
-        /*
-        ---------------------------------------------------------------------------------------------
-            EN PRIMER LUGAR, EN PHPMYADMIN HABRÍA QUE DECLARAR LA FUNCIÓN:
-        ---------------------------------------------------------------------------------------------
-            DELIMITER $$
-            CREATE FUNCTION enésimo_producto_más_repetido(N INT)
-            RETURNS INT
-            BEGIN
-                DECLARE result INT;
-                SET N = N - 1;
-                SELECT Codigo INTO result
-                FROM venta
-                ORDER BY Cantidad DESC
-                LIMIT N, 1;
-                RETURN result;
-            END$$
-            DELIMITER ;
-        ---------------------------------------------------------------------------------------------
-            EL valor de la variable direccion_producto sería el resultado de la consulta:
-        ---------------------------------------------------------------------------------------------
-            SELECT `Img` FROM producto WHERE `Codigo`=enésimo_producto_más_repetido(i);
-        ---------------------------------------------------------------------------------------------
-            EL SIGUIENTE CÓDIGO ES SOLO DE PRUEBA:    
-        */
-
-        //LA CONSULTA SE ENCUENTRA EN EL ARCHIVO runQuery.php
-        //PARA ENVIAR UNA VARIABLE SOLO AGREGAMOS A "../php/runQuery.php LA LÍNEA: ?variable="+variable
-
-        //direccion_producto = "../imagenes/21.png";
     } else {
-        
-
 if(CategoríaSeleccionada==" Navidad"){
 CategoríaSeleccionada=CategoríaSeleccionada.substring(1);
 console.log("CATEGORÍA: "+"X"+CategoríaSeleccionada+"X");
@@ -117,25 +58,8 @@ let myData = myAsyncFunction(CategoríaSeleccionada);
             seccion_productos.appendChild(div_aux);
         }
         );
-        /* 
-        ---------------------------------------------------------------------------------------------
-            EL valor de la variable direccion_producto sería el resultado de la consulta:
-        ---------------------------------------------------------------------------------------------
-            direccion_producto=SELECT Ruta_Imagen FROM producto WHERE Categoría=CategoríaSeleccionada LIMIT i-1, 1;
-            NOTA: PARA LA CONSULTA, ALADO DE LIMIT DEBE ESTAR UN NÚMERO ESPECÍFICO, ES DECIR, EL RESULTADO DE i-1
-        ---------------------------------------------------------------------------------------------
-            EL SIGUIENTE CÓDIGO ES SOLO DE PRUEBA:     
-        */
-        // if (CategoríaSeleccionada == "Cumpleaños") {
-        //     direccion_producto = "https://d320djwtwnl5uo.cloudfront.net/recetas/share/share_fsr8al91ct_confeti.jpg";
-        // } else {
-        //     direccion_producto = "https://cdn0.bodas.com.mx/article-real-wedding/799/3_2/1280/jpg/1466243.webp";
-        // }
     }
-
-
 }
-
 function myAsyncFunction(imagen) {
     const encodedImagen = encodeURIComponent(imagen);
     return new Promise((resolve, reject) => {
@@ -166,8 +90,6 @@ function esUrlValida(url) {
   const expresionRegular = /^(https?|http):\/\/[^\s/$.?#].[^\s]*$/i;
   return expresionRegular.test(url);
 }
-
-
 function colorTextoANegro(event) {
   let entrada_texto = event.target;
   entrada_texto.style.color = "black";
@@ -178,11 +100,6 @@ function colorTextoANegro(event) {
 }
 function opcionesPastel(event) {
     let tabla = document.querySelector(".tabla_info");
-    
-    
-
-  
-  
   if (event.target.id == "rec") {
     tamaño1 = "Mediana (35-40 personas)";
     tamaño2 = "Extra grande (100 personas)";
@@ -243,8 +160,6 @@ function opcionesPastel(event) {
     `);
     }
   }
-
-
   if (document.getElementById("normal") == null) {
     tabla.appendChild(div_fila);
     div_fila.insertAdjacentHTML("afterend", `
