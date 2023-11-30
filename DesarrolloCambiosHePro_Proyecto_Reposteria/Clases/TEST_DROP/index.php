711b0dd31cf1f401e8aea60ed0114f322b04e777
Antho-321 <anthonyluisluna225@gmail.com>
2023-11-30 17:21:56 -0500

    Cambiossss

---
+++ DesarrolloCambiosHePro_Proyecto_Reposteria/Clases/TEST_DROP/index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <script src="script.php"></script>
+</head>
+<body>
+  <div id="droppable" ondrop="alert('You dropped something!')">
+    Drop something here
+  </div>
+</body>
+</html>
