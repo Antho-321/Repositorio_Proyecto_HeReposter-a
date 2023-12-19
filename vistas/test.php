90cc488f839cd9df4fd875a5c13b20a3c73d4bc9
Antho-321 <anthonyluisluna225@gmail.com>
2023-12-19 00:14:05 -0500

    elimino archivo innecesario

--- vistas/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$password = 'hola'; // replace 'YourPassword' with the actual password
-
-// Hash the password using PHP's built-in function
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-echo "Hashed Password: " . $hashed_password;
-?>
