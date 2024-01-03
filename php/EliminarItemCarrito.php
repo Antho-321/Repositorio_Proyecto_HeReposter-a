ca7ced16ac441b4b146727855f679e5472bcd61c
Antho-321 <anthonyluisluna225@gmail.com>
2024-01-02 22:06:39 -0500

    desde cero con laravel

--- php/EliminarItemCarrito.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../php/Conexion.php"); 
-$conexion = new Conexion;
-$id_canasta =$_POST['id_canasta'];
-$eliminar_item =$_POST['eliminar_item'];
-$conexion->OperSql("DELETE FROM `canasta` WHERE `id_canasta` = '$id_canasta';");
-$conexion->closeConnection();
-header('Location: ../vistas/CarritoDeCompras.php ');
-?>
