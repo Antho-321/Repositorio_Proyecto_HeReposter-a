fa8a1dd0eb31d9249ce0bad0f841ea0ed14c96a6
JohannaPuerchambud <jepuerchambudp@utn.edu.ec>
2024-01-31 20:36:27 -0500

    m

--- resources/views/administrador/pedidos.blade.php
+++ resources/views/administrador/pedidos.blade.php
@@ -33,13 +33,13 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="modalEditarLabel">Ingresar cliente</h1>
+                        <h1 class="modal-title fs-5" id="modalEditarLabel">Ingresar Pedido</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
 
                         <!-- modal para ingresar -->
-                        <form action="{{ route('AdministradorClientesIngresar') }}" method="POST">
+                        <form action="{{ route('AdministradorPedidosIngresar') }}" method="POST">
                             @csrf
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Cedula</label>
@@ -73,7 +73,7 @@
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary"
                                     data-bs-dismiss="modal">Cancelar</button>
-                                <button type="submit" class="btn btn-primary">Insertar Cliente</button>
+                                <button type="submit" class="btn btn-primary">Insertar Pedido</button>
                             </div>
                         </form>
                     </div>
@@ -84,7 +84,7 @@
 
         <div class="p-5 table-responsive">
             <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalRegistrar">Ingresar
-                cliente</button>
+                Pedido</button>
 
 
             <table class="table table-striped table-bordered table-hover">
@@ -104,7 +104,7 @@
                     @foreach ($datos as $item)
                         <tr>
 
-                            <th>{{ $item->cliente_id }}</th>
+                            <th>{{ $item->pedido_id }}</th>
                             <td>{{ $item->cedula }}</td>
                             <td>{{ $item->nombre_cliente }}</td>
                             <td>{{ $item->telefono }}</td>
@@ -113,24 +113,24 @@
                             <td>{{ $item->clave }}</td>
                             <td>
                                 <a href="" data-bs-toggle="modal"
-                                    data-bs-target="#modalEditar{{ $item->cliente_id }}"
+                                    data-bs-target="#modalEditar{{ $item->pedido_id }}"
                                     class="btn btn-warning btn-sm"><i class="fa-solid fa-pen-to-square"></i></a>
-                                <a href="{{ route('AdministradorClientesEliminar', $item->cliente_id) }}"
+                                <a href="{{ route('AdministradorPedidosEliminar', $item->pedido_id) }}"
                                     onclick="return res()" class="btn btn-danger btn-sm"><i
                                         class="fa-solid fa-trash"></i></a>
                             </td>
                             <!-- Modal  PARA EDITAR-->
-                            <div class="modal fade" id="modalEditar{{ $item->cliente_id }}" tabindex="-1"
+                            <div class="modal fade" id="modalEditar{{ $item->pedido_id }}" tabindex="-1"
                                 aria-labelledby="modalEditarLabel" aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h1 class="modal-title fs-5" id="modalEditarLabel">Modificar cliente</h1>
+                                            <h1 class="modal-title fs-5" id="modalEditarLabel">Modificar Pedido</h1>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                 aria-label="Close"></button>
                                         </div>
                                         <div class="modal-body">
-                                            <form action="{{ route('AdministradorClientesActualizar') }}"
+                                            <form action="{{ route('AdministradorPedidosActualizar') }}"
                                                 method="POST">
                                                 @csrf
                                                 <div class="mb-3">
@@ -138,7 +138,7 @@
                                                         class="form-label">Codigo</label>
                                                     <input type="text" class="form-control"
                                                         id="exampleInputPassword1" name="txtCodigo"
-                                                        value="{{ $item->cliente_id }}" readonly>
+                                                        value="{{ $item->pedido_id }}" readonly>
                                                 </div>
                                                 <div class="mb-3">
                                                     <label for="exampleInputPassword1"
