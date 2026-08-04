c5e825de7ba51fb0f9dceea94ac747365c0e97c5
Antho-321 <allunav@utn.edu.ec>
2026-08-04 22:24:29 +0000

    style: adjust navigation bar height for desktop and add filemtime cache-busting to stylesheet link while removing obsolete direct_link file.

--- resources/views/plantilla_cliente/new_plantilla.blade.php
+++ resources/views/plantilla_cliente/new_plantilla.blade.php
@@ -17,7 +17,8 @@
         href="//fonts.googleapis.com/css?family=Roboto:100,300,300i,400,500,600,700,900%7CRaleway:500">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    {{-- ?v=filemtime: Cloudflare cachea los assets 4h; el sufijo evita servir CSS viejo tras un cambio --}}
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ filemtime(public_path('css/style.css')) }}">
     @yield('estilo')
     <!--[if lt IE 10]>
     <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
@@ -78,7 +79,7 @@
                     <div class="rd-navbar-inner-outer">
                         <div class="rd-navbar-inner">
                             <!-- RD Navbar Panel-->
-                            <div class="rd-navbar-panel" style="height: 80px;">
+                            <div class="rd-navbar-panel">
                                 <!-- RD Navbar Toggle-->
                                 <button class="rd-navbar-toggle"
                                     data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
